4ea71b8fff94bfc873d5e15d0dcf6db3d9ec31a7
Julien D <julien@pocketsizesun.com>
2021-03-18 09:15:11 +0100

    feat: add Unity::Utils::DynamoService#count_all

--- lib/unity/utils/dynamo_service.rb
+++ lib/unity/utils/dynamo_service.rb
@@ -22,6 +22,21 @@ module Unity
         end
       end
 
+      def count_all(params)
+        count = 0
+        params = params.merge(select: 'COUNT')
+        loop do
+          result = @connection_pool.with do |conn|
+            conn.query(params)
+          end
+          count += result.count
+          break if result.last_evaluated_key.nil?
+
+          params[:exclusive_start_key] = result.last_evaluated_key
+        end
+        count
+      end
+
       def query(params)
         @connection_pool.with do |conn|
           Unity.logger&.debug "[DynamoService] execute query: #{params.inspect}"
