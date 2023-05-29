72d80ac8fdd680062a9ddf36c11f78283595c41d
Julien D <julien@pocketsizesun.com>
2023-05-29 13:09:30 +0200

    add event processor

---
+++ lib/unity/event_processor.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Unity
+  class EventProcessor
+    def initialize(&block)
+      @handlers = {}
+      instance_exec(&block) unless block.nil?
+    end
+
+    def on(type, klass = nil, &block)
+      @handlers[type] ||= []
+      @handlers[type] << klass || block
+    end
+
+    def call(event)
+      return if @handlers[event.type].nil?
+
+      @handlers[event.type].each do |handler|
+        handler.call(event)
+      end
+    end
+  end
+end
